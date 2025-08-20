78e6a2086315c74185942c781ed26fd098d1ad04
1freelipe <feliperodrigues_spfc@hotmail.com>
2025-08-20 12:23:12 -0300

    feat: middleware loginRequired, rotas de contato

--- routes.js
+++ routes.js
@@ -4,6 +4,9 @@ const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
 const contactController = require('./src/controllers/contactController')
 
+// Middleware
+const { loginRequired } = require('./src/middlewares/middleware');
+
 // Rotas da home
 route.get('/index', homeController.index);
 
@@ -12,6 +15,11 @@ route.get('/login/index', loginController.indexLogin);
 route.post('/login/initial', loginController.loginInitial);
 route.post('/login/register', loginController.loginRegister);
 route.get('/login/logout', loginController.loginLogout);
-route.get('/contatos', contactController.contact);
+
+// Rotas de contato
+route.get('/contato/index', loginRequired, contactController.contact);
+route.post('/contato/register', loginRequired, contactController.register);
+route.get('/contato/index/:id', loginRequired, contactController.editIndex);
+route.post('/contato/edit/:id', loginRequired, contactController.editContact);
 
 module.exports = route;
